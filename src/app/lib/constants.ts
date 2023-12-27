df97e1a0973bbf3a63bb8c50f1402f95a2730e2f
Rashawn Evans <Reva8390@gmail.com>
2023-12-27 12:02:16 -0500

    Create Constants

---
+++ src/app/lib/constants.ts
@@ -0,0 +1,10 @@
+import { Crypto } from "./definitions";
+
+export const ONE_DAY: number = 1000 * 60 * 60 * 24;
+export const cryptos: Crypto[] = [
+  { name: "bitcoin", ticker: "BTC" },
+  { name: "ethereum", ticker: "ETH" },
+  { name: "solana", ticker: "SOL" },
+  { name: "xrp", ticker: "XRP" },
+  { name: "tether", ticker: "USDT" },
+];
